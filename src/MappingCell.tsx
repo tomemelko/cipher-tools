2b97f89d625a980ee7455f0a96648d00ca1cbdea
Tom Emelko <tom.emelko@gmail.com>
2022-09-05 18:10:20 +0000

    (mostly) fix the PT cell width issue

--- src/MappingCell.tsx
+++ src/MappingCell.tsx
@@ -1,4 +1,5 @@
-import React, { ChangeEvent } from 'react';
+import { useSizeAwareness } from 'Hooks';
+import React, { ChangeEvent, RefObject } from 'react';
 import { Mapping, TextState } from './TextDisplay';
 
 export type MappingCellProps = TextState & {
@@ -30,6 +31,21 @@ function MappingCell(props: MappingCellProps) {
     return (inputText.match(new RegExp(char, 'g')) || []).length || '-';
   }
 
+  const tdRef: RefObject<HTMLTableCellElement> = React.createRef();
+  const [cellWidth, setCellWidth] = React.useState<number>();
+  const dimensions = useSizeAwareness();
+  React.useEffect(() => {
+    let multiplier: number;
+    if (dimensions.width < 500) {
+      multiplier = 1.1;
+    } else if (dimensions.width < 1500) {
+      multiplier = 1.075;
+    } else {
+      multiplier = 1.05;
+    }
+    setCellWidth((tdRef.current?.offsetWidth ?? 0) * multiplier);
+  }, [tdRef, dimensions.width]);
+
   return (
     <div className="mapCell">
       <table className="mapLetterTable">
@@ -38,13 +54,13 @@ function MappingCell(props: MappingCellProps) {
             {array.map((char) => <td className="freqLetter" key={`freq${char}`}>{freq(text, char)}</td>)}
           </tr>
           <tr>
-            {array.map((char) => <td className="mapLetter" key={`ct${char}`}>{char}</td>)}
+            {array.map((char) => <td ref={(array.length === 26 && char === 'A') || (array.length === 13 && (char === 'A' || char === 'N')) ? tdRef : null} className="mapLetter" key={`ct${char}`}>{char}</td>)}
           </tr>
         </tbody>
       </table>
       <div className="mapInputContainer">
         {array.map((char) => (
-          <div className="mapInput" key={`pt${char}`}>
+          <div className="mapInput" key={`pt${char}`} style={{ width: `${cellWidth}px` }}>
             <input
               placeholder="?"
               onFocus={((event: ChangeEvent<HTMLInputElement>) => event.target.select())}
